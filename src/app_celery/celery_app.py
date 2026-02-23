ce5a03c204661c166e3c95d3896aedc3b9c904f3
Choudhary Himanshu <himanshuch3003@gmail.com>
2026-02-23 20:46:51 +0530

    feat: Update project structure with Docker support and Redis integration

--- src/app_celery/celery_app.py
+++ src/app_celery/celery_app.py
@@ -1,11 +1,14 @@
+import os
 from celery import Celery
 from celery.signals import worker_process_init
 from src.core.database_sync import mongodb_sync
 
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
 celery_app = Celery(
     "music_ai",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0",
+    broker=redis_url,
+    backend=redis_url,
 )
 
 @worker_process_init.connect
